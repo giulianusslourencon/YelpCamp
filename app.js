a49839c9dfebf5399aa1c9bd9c9de4bde36bfa42
Psy <giulianolourencon@gmail.com>
2020-04-01 14:40:02 -0300

    trying to fix connection

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 // app.listen(3000, function() {
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("The YelpCamp Server has started!!!");
 });
